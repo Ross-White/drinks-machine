14d1eb76cdb7093291d42653ed595a583631afd3
Ross White <rosswhite@outlook.com>
2021-11-05 14:19:22 +0000

    Sold Out working

--- server/controllers/drinkControllers.js
+++ server/controllers/drinkControllers.js
@@ -3,18 +3,21 @@ const { Drink } = require('../models');
 const getDrinkController = async (req, res) => {
     console.log(req.body);
     const drink = await Drink.findOne({ name: req.body.name});
-    if (!drink) {
-        return res.status(400).json({ message: 'Cannot find drink' });
+    if (drink.quantity === 0 ) {
+        return res.status(400).json({ message: 'Sold Out!' });
     }
     res.json(drink)
 }
 
 const sellDrinkController = async (req, res) => {
     const drink = await Drink.findOne({name: req.body.name});
-    const updatedDrink = await Drink.findOneAndUpdate({name: req.body.name}, { quantity: drink.quantity - 1 }, { new: true });
-    if (!updatedDrink) {
-        return res.status(400).json({ message: 'Cannot find drink' });
+    if (drink.quantity <= 0 ) {
+        return res.status(400).json({ message: 'Sold Out!' });
     }
+    const updatedDrink = await Drink.findOneAndUpdate(
+        {name: req.body.name}, 
+        { quantity: drink.quantity - 1 }, 
+        { new: true });
     res.json(updatedDrink)
 }
 

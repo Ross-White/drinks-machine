39de279a37a4153d25eeabf5f879a61529322798
Ross White <rosswhite@outlook.com>
2021-11-07 16:42:57 +0000

    updating package.json

--- server/index.js
+++ server/index.js
@@ -20,6 +20,9 @@ async function startApolloServer() {
 
   if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    });
   }
 
   db.once('open', () => {
